ea66527f4c2290617db8d8b0f2c53b3c6f4fa62e
mike treml <miketreml@mac.com>
2020-11-04 14:34:42 -0600

    Added OrderOrderDetailProductVM

---
+++ GroupCapstone/Models/OrderOrderDetailProductVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupCapstone.Models
+{
+    public class OrderOrderDetailProductVM
+    {
+        public Order OrderVM { get; set; }
+        public OrderDetails OrderDetailsVM { get; set; }
+        public Product ProductVM { get; set; }
+    }
+}
