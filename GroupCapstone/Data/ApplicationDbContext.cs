e798ea4aa2da1c5dd68a6e089d9d09c91e682d8c
mike treml <miketreml@mac.com>
2020-11-04 14:53:27 -0600

    Added DbContext properties.

--- GroupCapstone/Data/ApplicationDbContext.cs
+++ GroupCapstone/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GroupCapstone.Models;
 
 namespace GroupCapstone.Data
 {
@@ -18,5 +19,12 @@ namespace GroupCapstone.Data
 			new	IdentityRole { Name = "Employee", NormalizedName = "EMPLOYEE" },
 			new IdentityRole { Name = "Customer", NormalizedName = "CUSTOMER" });
 		}
+		public DbSet<GroupCapstone.Models.Customer> Customer { get; set; }
+		public DbSet<GroupCapstone.Models.Employee> Employee { get; set; }
+		public DbSet<GroupCapstone.Models.Admin> Admin { get; set; }
+		public DbSet<GroupCapstone.Models.Order> Order { get; set; }
+		public DbSet<GroupCapstone.Models.Product> Products { get; set; }
+		public DbSet<GroupCapstone.Models.OrderDetails> OrderDetails { get; set; }
+		public DbSet<GroupCapstone.Models.OrderOrderDetailProductVM> OrderOrderDetailProductVM { get; set; }
 	}
 }
