57f6a6ae3f18a24477c9f1ceb7c6977a5cd76504
mike treml <miketreml@mac.com>
2020-11-05 22:08:15 -0600

    Shopping cart intial build. Some of the view will not be needed.

---
+++ GroupCapstone/Models/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupCapstone.Models
+{
+    public class ShoppingCart
+    {
+
+        [Key]
+        public int Id { get; set; }
+        
+        public int Qty { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+    }
+}
