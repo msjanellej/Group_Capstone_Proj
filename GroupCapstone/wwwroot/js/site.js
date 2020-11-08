87608d91836de638e5e91eacdb8973b5149b5d23
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-11-07 20:32:25 -0600

    Add navbar dropdown styling and functions, Logo placement

--- GroupCapstone/wwwroot/js/site.js
+++ GroupCapstone/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");     // Add :active modifier to button while dropdown is visible
+}
 
-// Write your JavaScript code.
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+       if (document.getElementsByClassName("dropdown-content").classList.contains('show')) {
+         document.getElementById("dropdown-content").classList.remove('show');
+       }
+    }
+  }
