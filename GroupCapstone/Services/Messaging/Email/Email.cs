e9e3f321389974737254b3e72bbbbf7fd3fbe64b
mike treml <miketreml@mac.com>
2020-11-08 20:45:30 -0600

    email string corrected to web address

--- GroupCapstone/Services/Messaging/Email/Email.cs
+++ GroupCapstone/Services/Messaging/Email/Email.cs
@@ -18,7 +18,7 @@ namespace GroupCapstone.Services.Messaging.Email
             string destFile = System.IO.Path.Combine("", "qrCode.pdf");
             System.IO.File.Copy(sourceFile, destFile, true);
            
-            string qrString = "https://groupcapstone.conveyor.cloud/" + order.Id.ToString();
+            string qrString = "https://groupcapstone.conveyor.cloud/Employees/ConfirmOrderComplete/" + order.Id.ToString();
             QRCodeWriter.CreateQrCodeWithLogo(qrString, "logo.png", 150).StampToExistingPdfPage("qrcode.pdf", 200, 200, 1);
             qrCode = QRCodeWriter.CreateQrCode(order.Id.ToString(), 500, QRCodeWriter.QrErrorCorrectionLevel.Medium).ToHtmlTag();
             return qrCode;
